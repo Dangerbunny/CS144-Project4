5fc9980351d8d19f0cabedc857ee343fb839b994
Raymond Cothern <rtcothern@gmail.com>
2015-02-25 13:20:01 -0800

    SuggestionProvider had accidental duplicate

--- WebContents/suggestion.js
+++ WebContents/suggestion.js
@@ -23,10 +23,13 @@ function AutoSuggestControl(oTextbox, oProvider) {
         }
     };
 
-    this.autosuggest = function (aSuggestions) {
-        if (aSuggestions.length > 0) {
-            this.typeAhead(aSuggestions[0]);
-        }
+    this.autosuggest = function (suggestList, showTypeAhead) {
+        if (suggestList.length > 0) {
+            if(showTypeAhead)
+                this.typeAhead(suggestList[0]);
+            this.showSuggestions(suggestList);
+        } else
+            this.hideSuggestions();
     };
 
     this.handleKeyUp = function (oEvent) {
@@ -69,11 +72,53 @@ function AutoSuggestControl(oTextbox, oProvider) {
                 asControl.textbox.value = target.firstChild.nodeValue;
                 asControl.hideSuggestions();
             } else if (event.type == "mouseover"){
-                
+                asControl.highlightSuggestion(target);
+            } else{
+                asControl.textbox.focus(); //return focus to the text box
             }
         };
     };
+
+    this.getLeft = function () {
+        var oNode = this.textbox;
+        var iLeft = 0;
+
+        while(oNode.tagName != "BODY") {
+            iLeft += oNode.offsetLeft;
+            oNode = oNode.offsetParent; 
+        }
+
+        return iLeft;
+    };
+
+    this.getTop = function () {
+        var oNode = this.textbox;
+        var iTop = 0;
+
+        while(oNode.tagName != "BODY") {
+            iTop += oNode.offsetTop;
+            oNode = oNode.offsetParent; 
+        }
+
+        return iTop;
+    };
+
+    this.showSuggestions = function (aSuggestions) {
+        var oDiv = null;
+        this.layer.innerHTML = "";
+
+        for (var i=0; i < aSuggestions.length; i++) {
+            oDiv = document.createElement("div");
+            oDiv.appendChild(document.createTextNode(aSuggestions[i]));
+            this.layer.appendChild(oDiv);
+        }
+
+        this.layer.style.left = this.getLeft() + "px";
+        this.layer.style.top = (this.getTop()+this.textbox.offsetHeight) + "px";
+        this.layer.style.visibility = "visible";
+    };
 }
+
 AutoSuggestControl.prototype.init = function () {
     var oThis = this;
     this.textbox.onkeyup = function (oEvent) {
@@ -90,18 +135,12 @@ function SuggestionProvider() {
     //any initializations needed go here
 }
 
-StateSuggestions.prototype.requestSuggestions = function (oAutoSuggestControl) {
-    var aSuggestions = [];
-    var sTextboxValue = oAutoSuggestControl.textbox.value;
+SuggestionProvider.prototype.requestSuggestions = function (oAutoSuggestControl) {
 
-    if (sTextboxValue.length > 0){
-        for (var i=0; i < this.states.length; i++) { 
-            if (this.states[i].indexOf(sTextboxValue) == 0) {
-                aSuggestions.push(this.states[i]);
-            } 
-        } 
-        oAutoSuggestControl.autosuggest(aSuggestions);
-    } 
+    var aSuggestions = new Array();
+
+    //determine suggestions for the control
+    oAutoSuggestControl.autosuggest(aSuggestions);
 };
 
 //--------------------------------------------------------------------------------------------------------
