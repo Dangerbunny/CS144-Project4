0f96fc877949a639e5ff913ad3e2a4752e056bad
Raymond Cothern <rtcothern@gmail.com>
2015-02-24 22:18:13 -0800

    Started proxy servlet

--- src/edu/ucla/cs/cs144/ProxyServlet.java
+++ src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,12 +1,20 @@
 package edu.ucla.cs.cs144;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 
 public class ProxyServlet extends HttpServlet implements Servlet {
        
@@ -14,6 +22,34 @@ public class ProxyServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+    	String query = request.getParameter("q");
+    	URL url = new URL("http://google.com/complete/search?output=toolbar&q="+query);
+    	HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    	String theString;
+    	try {
+    	     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+    	     theString = IOUtils.toString(in, Charset.defaultCharset().toString());
+    	}
+    	    finally {
+    	     urlConnection.disconnect();
+    	}
+    	PrintWriter writer = response.getWriter();
+    	
     }
+    
+    /**
+     * String query = request.getParameter("q");
+    	URL url = new URL("http://google.com/complete/search?output=toolbar&q="+query);
+    	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    	conn.setRequestMethod("GET");
+    	conn.setRequestProperty("Accept", "application/json");
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String inline = "";
+        while ((inline = br.readLine()) != null) {
+          sb.append(inline);
+        }
+        request.setAttribute("data", sb.toString());
+        request.getRequestDispatcher("/temp.jsp").forward(request, response);
+     */
 }
